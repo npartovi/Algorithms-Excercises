5204075b6f9d5297535c1a53d314ce5997aef013
npartovi <npartovi@gmail.com>
2018-05-01 23:12:43 -0700

    finished hash maps

--- LRU:HashMaps/lib/p05_hash_map.rb
+++ LRU:HashMaps/lib/p05_hash_map.rb
@@ -3,6 +3,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_reader :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -10,27 +11,46 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
+
   end
 
   def set(key, val)
+    resize! if @count == num_buckets
+
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1
+    bucket(key).remove(key)
+
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |link|
+        yield [link.key, link.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,9 +62,18 @@ class HashMap
   end
 
   def resize!
+    temp_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new}
+
+    temp_store.each do |bucket|
+      bucket.each do |link|
+        set(link.key, link.val)
+      end
+    end
+
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
