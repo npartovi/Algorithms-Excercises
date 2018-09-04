6d940225776ff9c87a00276e149b6d301943a3f4
npartovi <npartovi@gmail.com>
2018-09-04 11:42:42 -0700

    binary trees done

--- algocasts/exercises/levelwidth/index.js
+++ algocasts/exercises/levelwidth/index.js
@@ -11,6 +11,29 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+
+    let counter = 0
+    const arr = [root, "stop"]
+    const results = []
+
+    while(arr.length > 1){
+
+        const node = arr.shift()
+
+        if(node === "stop"){
+            results.push(counter)
+            counter = 0
+            arr.push('stop')
+        }else{
+            arr.push(...node.children)
+            counter += 1
+        }
+    }
+
+    results.push(counter)
+    return results
+
+}
 
 module.exports = levelWidth;
