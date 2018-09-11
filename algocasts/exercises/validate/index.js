b4ec83ca165f93ae7225793d108251c6e5a4ea5b
npartovi <npartovi@gmail.com>
2018-09-11 13:14:51 -0700

    added another solition for binary tree checker

--- algocasts/exercises/validate/index.js
+++ algocasts/exercises/validate/index.js
@@ -29,4 +29,16 @@ function validate(node, min = null, max = null) {
 
 }
 
+// alternative solution
+
+// var isValidBST = function(root, min, max){
+//     return isValidBSTHelper(root, -Infinity, Infinity);
+// };
+
+// let isValidBSTHelper = function(root, min, max){
+//     if(root === null) return true;
+//     if(root.val <= min || root.val >= max) return false;
+//     return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+// }
+
 module.exports = validate;
